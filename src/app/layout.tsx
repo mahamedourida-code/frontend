9897c566db77dd33ddd9f340f56fae4358021d69
baraourida <baraourida@gmail.com>
2026-05-29 00:56:38 +0100

    cursor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import "../styles/mobile-nav.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ProcessingStateProvider } from "@/contexts/ProcessingStateContext";
 import { AuthProvider } from "@/contexts/AuthContext";
-import { CustomCursor } from "@/components/CustomCursor";
 import { Toaster } from "sonner";
 
 const inter = Inter({
@@ -96,7 +95,6 @@ export default function RootLayout({
               <div className="relative z-10">
                 {children}
               </div>
-              <CustomCursor />
               <Toaster
                 richColors
                 position="top-right"
